7cfae835e367d36f06468b998466404d2d4e5e51
Glitch (electric-result) <none>
2020-01-14 01:50:36 +0000

    🌾🐇 Updated with Glitch

--- index.js
+++ index.js
@@ -1,55 +1,72 @@
-const { CommandoClient } = require('discord.js-commando');
-const { Structures } = require('discord.js');
-const path = require('path');
-const { prefix, token } = require('./config.json');
+const app = require("express")();
 
-Structures.extend('Guild', Guild => {
-  class MusicGuild extends Guild {
-    constructor(client, data) {
-      super(client, data);
-      this.musicData = {
-        queue: [],
-        isPlaying: false,
-        nowPlaying: null,
-        songDispatcher: null
-      };
-      this.triviaData = {
-        isTriviaRunning: false,
-        wasTriviaEndCalled: false,
-        triviaQueue: [],
-        triviaScore: new Map()
-      };
-    }
-  }
-  return MusicGuild;
+const http = require("http");
+app.get("/", (request, response) => {
+  console.log(Date.now() + "Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.inv.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+// this is to keep alive your bot, just add it to uptime robot to ping it every 5 minutes
+
+const { Client, Collection, Util } = require("discord.js");
+const { config } = require("dotenv");
+const fs = require("fs");
+const { promptMessage } = require("./function.js");
+const Discord = require("discord.js");
+const { PREFIX, TOKEN, STATUS } = require("./config.js");
+const bot = new Discord.Client();
+
+const client = new Client({
+  disableEveryone: true
+});
+//assest ke ander img h wo daalni h canvas h. w   image kahan dalni hai???
+client.commands = new Collection();
+client.aliases = new Collection();
+
+client.categories = fs.readdirSync("./commands/");
+
+config({
+  path: __dirname + "/.env"
 });
 
-const client = new CommandoClient({
-  commandPrefix: prefix,
-  owner: '183647046564184065' // change this to your Discord user ID
+["command"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
 });
 
-client.registry
-  .registerDefaultTypes()
-  .registerGroups([
-    ['music', 'Music Command Group'],
-    ['gifs', 'Gif Command Group'],
-    ['other', 'random types of commands group'],
-    ['guild', 'guild related commands']
-  ])
-  .registerDefaultGroups()
-  .registerDefaultCommands()
-  .registerCommandsIn(path.join(__dirname, 'commands'));
+client.on("ready", () => {
+  console.log(`Hi, ${client.user.username} is now online!`);
 
-client.once('ready', () => {
-  console.log('Ready!');
-  client.user.setActivity('!help for commands', 'WATCHING');
+  setInterval(() => {
+    client.user.setActivity(`${STATUS}`, { type: "Watching" });
+  }, 5000);
 });
 
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find(c => c.name === 'general'); // change this to the channel you want to send the greeting to
-  if (!channel) return;
-  channel.send(`Welcome ${member}!`);
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(PREFIX)) return;
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+
+  const args = message.content
+    .slice(PREFIX.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd.length === 0) return;
+
+  let command = client.commands.get(cmd);
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  if (command) command.run(client, message, args);
 });
 
-client.login(token);
+ 
+
+
+client.login(TOKEN);
+//now make a new file namee config.js and put token pre and your bot status there
